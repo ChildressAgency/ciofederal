5070f2c5e6dbc3b88cbf067a792e041fec7982c8
childressdev <jcampbell@childressagency.com>
2017-08-10 11:37:09 -0400

    header styled

--- js/ciofederal-scripts.js
+++ js/ciofederal-scripts.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function($){
-  
+  $('.scroller').on('click', function(e){
+    e.preventDefault;
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }    
+  }); 
+
+  if(typeof $.fn.fullpage == 'function'){
+    $('#hp-main').fullpage({
+      responsiveHeight:600,
+      verticalCentered:false,
+      slideSelector:'.fullpage-slide',
+      //paddingTop:'71px'
+    });
+  }
 });
